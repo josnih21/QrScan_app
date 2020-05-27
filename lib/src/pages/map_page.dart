3d83d7b921052d8c685f220055e6f1043987e650
José Ángel <josnih@gmail.com>
2020-05-27 17:15:54 +0100

    adding map style change

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:qrscanner_sql/src/models/scan_models.dart';
-import 'package:latlong/latlong.dart';
-class MapPage extends StatelessWidget {
+
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
   final MapController map = new MapController();
+  String tipoMapa = 'streets-v11';
   @override
   Widget build(BuildContext context) {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
@@ -19,6 +25,7 @@ class MapPage extends StatelessWidget {
         ],
       ),
       body: _createFlutterMap(scan),
+      floatingActionButton: _createButton(context),
     );
   }
 
@@ -43,7 +50,7 @@ class MapPage extends StatelessWidget {
                   '{id}/tiles/{z}/{x}/{y}@2x?access_token={accessToken}',
         additionalOptions: {
         'accessToken':'pk.eyJ1Ijoiam9zbmloIiwiYSI6ImNrYXBncno4czFkbW0ycnFtMjQzc3gyYzEifQ.APwKt9IGFXculR0E_W-KWg',
-        'id': 'mapbox/streets-v11' //streets,dark,light, outdoors, satellite
+        'id': 'mapbox/$tipoMapa' //streets,dark,light, outdoors, satellite
         }
     );
   }
@@ -60,6 +67,24 @@ class MapPage extends StatelessWidget {
           )
         )
       ]
+    );
+  }
+
+  _createButton(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: (){
+        print('hola');
+        if(tipoMapa == 'streets-v11'){
+          tipoMapa = 'satellite-streets-v11';
+        }
+        else{
+          tipoMapa = 'streets-v11';
+        }
+
+        setState((){});
+      },
+      child: Icon(Icons.repeat),
+      backgroundColor: Theme.of(context).primaryColor,
     );
   }
 }
